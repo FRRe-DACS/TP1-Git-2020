b7bb778acf2fae4d40ad32a6a637266716191907
JazminTeng <jazminteng@gmail.com>
2020-04-11 19:30:31 -0300

    Agregue funcion UpperCase

--- grupo3/HolaMundo/Program.cs
+++ grupo3/HolaMundo/Program.cs
@@ -8,5 +8,10 @@ namespace grupo3
         {
             Console.WriteLine("Hello World!");
         }
+	static void UpperC(string inputmsj)
+        {
+            inputmsj=inputmsj.ToUpper();
+            Console.WriteLine(inputmsj);
+        }
     }
 }
