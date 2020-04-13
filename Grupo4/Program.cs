408694a1102826e90aec7f319dcc5b3c1a1d9eaa
andrikivan <ivanandrik@outlook.com.ar>
2020-04-13 15:43:52 -0300

    Program.cs imprime somos el grupo 4

--- Grupo4/Program.cs
+++ Grupo4/Program.cs
@@ -6,7 +6,7 @@ namespace Grupo4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Somos el grupo 4");
         }
     }
 }
