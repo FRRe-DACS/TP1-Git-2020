1982fb15b33faad9421e7b3a6234caf93076c4f1
Miguel <miguelbritos91@gmail.com>
2020-04-13 17:38:29 -0300

    Funcion uppercase G4

--- Grupo4/Program.cs
+++ Grupo4/Program.cs
@@ -7,6 +7,29 @@ namespace Grupo4
         static void Main(string[] args)
         {
             Console.WriteLine("Somos el grupo 4");
+            
+            string mayus = uppercase("test de la funcion uppercase");
+            Console.WriteLine(mayus);
+
+        }
+
+        
+        /*
+           FUNCION PARA TRANSFORMAR TEXTO EN MAYUSCULAS
+           AUTOR: MIGUEL BRITOS
+        */
+        static string uppercase(string palabra)
+        {
+            string input = palabra;
+            string result = string.Empty;
+            for(int i=0;i<input.Length;i++){
+                if(input[i]>='a'&&input[i]<='z'){
+                    result+=(char)(input[i]-32);
+                }else{
+                    result+=input[i];
+                }
+            }
+            return result;
         }
     }
 }
