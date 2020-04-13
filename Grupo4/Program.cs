9611763b6bcd5a0ae82100a5aa681ac60f92716e
andrikivan <ivanandrik@outlook.com.ar>
2020-04-13 20:29:42 -0300

    X corregido: Se agregó la función sumar numero más su factorial

--- Grupo4/Program.cs
+++ Grupo4/Program.cs
@@ -18,8 +18,8 @@ namespace Grupo4
             numero = int.Parse(Console.ReadLine());
             Console.WriteLine("\nEl factorial del numero es: " + factorial(numero));
 
-            Console.Write("\nEsta es la suma de el número dado más su factorial: ");
-            Console.WriteLine("\nLa suma da: " + suma(numero,factorial(numero)));
+            Console.Write(" \nEsta es la suma de el número dado más su factorial: ");
+            Console.WriteLine(" \nLa suma da: " + suma(numero,factorial(numero)));
 
         }
 
