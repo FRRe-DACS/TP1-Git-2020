bef54b4d9a4ffe7d8382148ccb4b0d60934a073c
mfacu17 <mfacu98@gmail.com>
2020-04-13 17:02:15 -0300

    Funcion factorial

--- Grupo4/Program.cs
+++ Grupo4/Program.cs
@@ -4,9 +4,27 @@ namespace Grupo4
 {
     class Program
     {
+        static int factorial(int n)
+        {
+            int fac = 1;
+
+            for(int i = 1; i<=n;i++)
+                fac = fac * i;
+            
+            return fac;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Somos el grupo 4");
+            int numero;
+
+            for(int x = 1;x<=10;x++){
+                Console.Write("\nIngrese el número");
+                numero = int.Parse(Console.ReadLine());
+
+                Console.Write("\nEl factorial del numero es: " + factorial(numero));
+            }
+
+            Console.ReadKey();
         }
     }
 }
